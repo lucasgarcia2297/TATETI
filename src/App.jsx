67a34f0f9e162949c9b92b7af732d9fc0209391c
lucasgarcia2297 <lucas.garcia2297@gmail.com>
2024-07-02 14:18:05 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ const Square = ({children, isSelected, updateBoard, index})=>{
     </div>
   )
 }
+
 const WINNER_COMBOS=[
   [0, 1, 2], [3, 4, 5], [6, 7, 8], // Filas
   [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columnas
